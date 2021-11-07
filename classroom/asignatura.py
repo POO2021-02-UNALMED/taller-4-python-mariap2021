f9987542320e42a27ac6890065d29e22da60b42c
mariap2021 <92173632+mariap2021@users.noreply.github.com>
2021-11-06 19:21:57 -0500

    one

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,3 +3,12 @@ class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+
+    
+
+    def __str__(self):
+       anadir= self._nombre + " "+ self._salon
+       return anadir
+
+
+
